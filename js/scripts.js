aaa38592d981f744f2617b455d842c0b2f223782
Zuzanna <zuzanna.zuzewicz@gmail.com>
2017-11-22 15:20:49 +0200

    Added basic project structure

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var a = 12,
+	h = 34,
+	triangleArea;
+
+triangleArea = a*h/2;
+
+console.log('The area of a triangle with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
+
+alert(triangleArea);
