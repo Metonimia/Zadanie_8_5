17eaf533c2ad6f0a8a9dd7c36f212ffcd667f150
Zuzanna <zuzanna.zuzewicz@gmail.com>
2017-11-23 15:27:15 +0200

    Updated scripts.js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,6 @@
 var a = 12,
 	h = 34,
-	triangleArea;
-
-triangleArea = a*h/2;
+	triangleArea = a * h / 2;
 
 console.log('The area of a triangle with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea + '.');
 
